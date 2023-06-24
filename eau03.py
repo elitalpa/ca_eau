3ab7302ac9455c717ec05849d58ea31fc9bafe89
elitalpa <elitalpa@gmail.com>
2023-06-24 15:48:32 +0200

    eau03.py - improving indentation

--- eau03.py
+++ eau03.py
@@ -12,8 +12,10 @@ import sys
 def get_fibonacci_number(fibonacci_index):
     fibonacci_sequence_list = [0, 1]
     fibonacci_index_int = int(fibonacci_index)
+
     for number_index in range(2, fibonacci_index_int + 1):
         next_fibonacci_number = fibonacci_sequence_list[number_index - 2] + fibonacci_sequence_list[number_index - 1]
+        
         fibonacci_sequence_list.append(next_fibonacci_number)
 
     return fibonacci_sequence_list[fibonacci_index_int]
