30140c192333eeb979e50e51f3542fee6a5396ef
elitalpa <elitalpa@gmail.com>
2023-06-24 15:43:00 +0200

    eau03.py - fibonacci sequence exercise is done

--- eau03.py
+++ eau03.py
@@ -6,12 +6,36 @@
 #
 # Définition: La suite de Fibonacci est une suite d'entiers dans laquelle chaque terme est la somme des deux termes qui le précèdent.
 
+import sys
+
 ### Functions ###
+def get_fibonacci_number(fibonacci_index):
+    fibonacci_sequence_list = [0, 1]
+    fibonacci_index_int = int(fibonacci_index)
+    for number_index in range(2, fibonacci_index_int + 1):
+        next_fibonacci_number = fibonacci_sequence_list[number_index - 2] + fibonacci_sequence_list[number_index - 1]
+        fibonacci_sequence_list.append(next_fibonacci_number)
+
+    return fibonacci_sequence_list[fibonacci_index_int]
+
+def handle_argument_errors():
+    if len(sys.argv) != 2:
+        print(-1)
+        print("error: only one argument can be entered")
+        exit()
+    if not sys.argv[1].isdigit():
+        print(-1)
+        print("error: only a positive integer number can be accepted")
+        exit()
 
 ### Error Handling ###
+handle_argument_errors()
 
 ### Parsing ###
+fibonacci_index_input = sys.argv[1]
 
 ### Problem Solving ###
+fibonacci_number_result = get_fibonacci_number(fibonacci_index_input)
 
 ### Result ###
+print(fibonacci_number_result)
