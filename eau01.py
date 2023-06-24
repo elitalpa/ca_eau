381974722d2bab3d6e227e685d4f1ce5bde9faa1
elitalpa <elitalpa@gmail.com>
2023-06-24 13:46:49 +0200

    eau01.py - combinations of 2 numbers in ascending order exercise is done

--- eau01.py
+++ eau01.py
@@ -3,11 +3,49 @@
 # de deux nombre entre 00 et 99 dans l’ordre croissant.
 
 ### Functions ###
+def generate_combinations_of_2_numbers_ascending():
+    iteration_count = 0
+    digit_a = 0
+    digit_b = 0
+    digit_c = 0
+    digit_d = 1
+    #all_combinations = ""
+    valid_combinations = ""
+
+    while(iteration_count < 9999):
+        #all_combinations += f"{digit_a}{digit_b} {digit_c}{digit_d}, "
+
+        if int(f"{digit_a}{digit_b}") < int(f"{digit_c}{digit_d}"):
+            valid_combinations += f"{digit_a}{digit_b} {digit_c}{digit_d}, "
+        
+        iteration_count += 1
+        
+        if digit_a == 9 and digit_b == 9 and digit_c == 9 and digit_d == 9:
+            break
+        elif digit_b == 9 and digit_c == 9 and digit_d == 9:
+            digit_b = 0
+            digit_c = 0
+            digit_d = 0
+            digit_a += 1
+        elif digit_c == 9 and digit_d == 9:
+            digit_c = 0
+            digit_d = 0
+            digit_b += 1
+        elif digit_d == 9:
+            digit_d = 0
+            digit_c += 1
+        else:
+            digit_d += 1
+    
+    #return all_combinations[:-2]
+    return valid_combinations[:-2]
 
 ### Error Handling ###
 
 ### Parsing ###
 
 ### Problem Solving ###
+combinations_of_2_numbers_ascending = generate_combinations_of_2_numbers_ascending()
 
 ### Result ###
+print(combinations_of_2_numbers_ascending)
