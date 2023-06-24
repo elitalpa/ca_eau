54ca77576f3e20be0d4a0c7537844d88c35bbdfc
elitalpa <elitalpa@gmail.com>
2023-06-24 13:22:59 +0200

    eau00.py - combinations of 3 digits exercise is done

--- eau00.py
+++ eau00.py
@@ -10,11 +10,42 @@
 # 000 n’est pas là parce que cette combinaison ne comporte pas exclusivement des chiffres différents les uns des autres.
 
 ### Functions ###
+def generate_combinations_of_3_digits():
+    iteration_count = 0
+    digit_a = 0
+    digit_b = 0
+    digit_c = 1
+    #all_combinations = ""
+    valid_combinations = ""
+
+    while(iteration_count < 999):
+        #all_combinations += f"{digit_a}{digit_b}{digit_c}\n"
+        
+        if digit_a < digit_b < digit_c:
+            valid_combinations += f"{digit_a}{digit_b}{digit_c}, "
+        
+        iteration_count += 1
+
+        if digit_a == 9 and digit_b == 9 and digit_c == 9:
+            break
+        elif digit_b == 9 and digit_c == 9:
+            digit_b = 0
+            digit_c = 0
+            digit_a += 1
+        elif digit_c == 9:
+            digit_c = 0
+            digit_b += 1
+        else:
+            digit_c += 1
+
+    return valid_combinations[:-2]
 
 ### Error Handling ###
 
 ### Parsing ###
 
 ### Problem Solving ###
+combinations_result = generate_combinations_of_3_digits()
 
 ### Result ###
+print(combinations_result)
