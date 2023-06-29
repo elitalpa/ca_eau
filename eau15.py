d2c3d2f8bef17c19f88034539d9c93ad5c27f623
elitalpa <elitalpa@gmail.com>
2023-06-29 23:46:54 +0200

    eau15.py - the water trial is done

--- eau15.py
+++ eau15.py
@@ -2,12 +2,20 @@
 # Créez un programme qui célèbre votre victoire.
 # Note : [] est à remplacer par un adjectif de votre choix (facile ?)
 
-### Functions ###
+import random
 
-### Error Handling ###
+### Function ###
+def get_victory_message():
+    adjective_of_victory = ["aquatique", "naviguable", "maritime"]
+    victory_index = random.randint(0,2)
+    return f"J’ai terminé l’Épreuve de la Terre et c’était {adjective_of_victory[victory_index]}."
+
+### Error ###
 
 ### Parsing ###
 
 ### Problem Solving ###
+victory_message = get_victory_message()
 
 ### Result ###
+print(victory_message)
